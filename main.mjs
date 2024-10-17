a82712a46a7bd7aa8c88ffb0219e5176c35fdda5
SA <sakshamagrawal2908@gmail.com>
2024-10-17 14:16:47 +0530

    html work

---
+++ main.mjs
@@ -0,0 +1,15 @@
+import {add,subtract,PI} from "./newfile.mjs";
+
+import * as jadoo from "./newfile.mjs";
+
+import multiply from "./newfile.mjs";
+
+export {add,subtract,PI,multiply};
+
+console.log("Add: ", jadoo.add(5, 3));
+
+console.log("Subtract: ", jadoo.subtract(5, 3));
+
+console.log("PI: ", jadoo.PI);
+
+console.log("Multiply: ", multiply(5, 3));
